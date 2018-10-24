e7d5010e44c9d756cee23bd8b35c407dad4c7816
yaying ye <yayingye@yayings-MacBook-Pro.local>
2018-10-23 21:04:05 -0700

    stable version

--- fatfinger/fatfinger.js
+++ fatfinger/fatfinger.js
@@ -1,24 +1,147 @@
 //Test
 var input = [];
 var output = [];
+var temp = [];
 function firstFunction(){
   input.push(1);
+  temp.push(1);
+  mapChar();
   setTimeout(function(){submitFunction();}, 1000);
+  
 
   
 }
 function secondFunction(){
   input.push(2);
+  temp.push(2);
+  mapChar();
   setTimeout(function(){submitFunction();}, 1000);
 }
 function thirdFunction(){
   input.push(3);
+  temp.push(3);
+  mapChar();
   setTimeout(function(){submitFunction();}, 1000);
   
 }
 function forthFunction(){
   input.push(4);
+  temp.push(4);
+  mapChar();
   setTimeout(function(){submitFunction();}, 1000);
+  
+}
+function mapChar() {
+  if (JSON.stringify(temp)==JSON.stringify([1])) {  
+    document.getElementById("point").innerHTML = "a"
+    
+  }
+ else if (JSON.stringify(temp)==JSON.stringify([1,1])) {
+    
+    document.getElementById("point").innerHTML = "b"
+    
+  }
+   else if (JSON.stringify(temp)==JSON.stringify([1,1,1])) {
+    document.getElementById("point").innerHTML = "c"
+    
+  }
+   else if (JSON.stringify(temp)==JSON.stringify([1,1,1,1])) {
+    document.getElementById("point").innerHTML = "d"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([2])) {
+    
+    document.getElementById("point").innerHTML = "e"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([2,2])) {
+   document.getElementById("point").innerHTML = "f"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([2, 2, 2])) {
+   document.getElementById("point").innerHTML = "g"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([2,2,2,2])) {
+   document.getElementById("point").innerHTML = "h"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([3])) {
+    document.getElementById("point").innerHTML = "i"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([3,3])) {
+   document.getElementById("point").innerHTML = "j"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([3,3,3])) {
+    document.getElementById("point").innerHTML = "k"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([3,3,3,3])) {
+    document.getElementById("point").innerHTML = "l"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([4])) {
+   document.getElementById("point").innerHTML = "m"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([4, 4])) {
+    document.getElementById("point").innerHTML = "n"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([4, 4, 4])) {
+    document.getElementById("point").innerHTML = "o"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([4, 4, 4, 4])) {
+    document.getElementById("point").innerHTML = "p"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([1, 2])) {
+    document.getElementById("point").innerHTML = "q"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([2,1])) {
+   document.getElementById("point").innerHTML = "r"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([2, 4])) {
+    document.getElementById("point").innerHTML = "s"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([4, 2])) {
+    document.getElementById("point").innerHTML = "t"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([3, 4])) {
+    document.getElementById("point").innerHTML = "u"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([4, 3])) {
+    document.getElementById("point").innerHTML = "v"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([1, 3])) {
+    document.getElementById("point").innerHTML = "w"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([3, 1])) {
+    document.getElementById("point").innerHTML = "x"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([3, 2])) {
+    document.getElementById("point").innerHTML = "y"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([2, 3])) {
+    document.getElementById("point").innerHTML = "z"
+    
+  }
+  else if (JSON.stringify(temp)==JSON.stringify([1, 4])) {
+    document.getElementById("point").innerHTML = " "
+    
+  }
   
 }
 function submitFunction() {
@@ -163,4 +286,6 @@ function submitFunction() {
     
   }
   input = [];
+  temp = [];
+  document.getElementById("point").innerHTML = "_"
 }
